11af40062bef19079659e2af83de210fac37c3a9
Anton <anton98156789@gmail.com>
2023-02-11 15:49:44 +0300

    1

--- program.java
+++ program.java
@@ -37,19 +37,30 @@
  * Если в массиве есть числа, равные заданному, нужно перенести эти элементы в конец массива.
  * Таким образом, первые несколько (или все) элементов массива должны быть отличны от заданного, а остальные - равны ему.
  */
+
+
+
 public class program {
     public static void main(String[] args) {
         int[] array = {3, 2, 2, 3};
-        swap(array, 3);
-        System.out.println(array); // [2, 2, 3, 3]
+        // int val = 3;
+        swap(array, val:3);
+        System.out.println(Arrays.ToString(array)); // [2, 2, 3, 3]
     }
 
     private static void swap(int[] array, int val) {
-        int temp = 0;
+        int count = 1;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == val && array[array.length - 1] != 3) {
-                temp = array[i];
-                array[array.length - 1] = array[i];
+
+            int temp = 0;
+            if (array[i] == val && array.length - count > i) {
+
+                while (array[array.length-count]==val){
+                    count++;
+                }
+                temp = array[array.length-count];
+                array[array.length - count] = array[i];
+                count++;
                 array[i] = temp;
             }
         }
